bf2cde1be772034de036ed4467ca8aa35158f5e3
Philipp Jantscher <philipp.jantscher@student.tugraz.at>
2015-12-09 06:23:43 -0800

    added makefile in tests (there is a bug at compilation) and simulator execution file

--- code/vsim/execute_single.sh
+++ code/vsim/execute_single.sh
@@ -1,1 +1,1 @@
-./DefaultConfig-sim-debug +vcd +vcd_name=output/super.vcd +max-cycles=100000000 +load=output/rv64ui-p-sra.hex | /home/zaepo/lowRISC/lowrisc-chip/riscv/bin/spike-dasm  >output/rv64ui-p-sra.verilator.out && [ $PIPESTATUS -eq 0 ]
+./DefaultConfig-sim-debug +vcd +vcd_name=output/hello.vcd +max-cycles=100000000 +load=/home/zaepo/iaikgit/2015_master_jantscher/code/fpga/board/kc705/examples/hello.hex | /home/zaepo/iaikgit/2015_master_jantscher/code/riscv/bin/spike-dasm  >output/hello.verilator.out && [ $PIPESTATUS -eq 0 ]
