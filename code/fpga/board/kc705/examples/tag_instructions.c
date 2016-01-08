17f65757e7cde0bf821088b2ea8b1f4584083275
Philipp Jantscher <philipp.jantscher@student.tugraz.at>
2016-01-08 09:53:34 -0800

    LTAG instructio now working (bug in decode table). Added presets for GTKWave and prebuilt linux

--- code/fpga/board/kc705/examples/tag_instructions.c
+++ code/fpga/board/kc705/examples/tag_instructions.c
@@ -7,7 +7,7 @@
  long a[10];
 
 int main() {
- a[0] = 0x01;
+ a[0] = 0xDDDDDDFEFEEFFFFF;
  a[1] = 0x02;
  a[2] = 0x03;
  a[3] = 0x04;
@@ -15,16 +15,32 @@ int main() {
   int test = 0;
  // for(i=0; i<10; i++) {
     int temp = 0;
-int tag_in = 0x02;
- asm volatile ("ltag %0, 0(%1)":"=r"(temp):"r"(a));
-temp = 0;
-  asm volatile ("stag %0, 0(%1)"
-                :
-                :"r"(tag_in), "r"(a)
- );
-    asm volatile ("ltag %0, 0(%1)":"=r"(temp):"r"(a));
-    i += temp;
- if(temp == 0x02)
+int tag_in = 0x08;
+ //asm volatile ("ltag %0, 0(%1)":"=r"(temp):"r"(a));
+//temp = 0;
+
+//Do some other stuff to flush the registers
+long bla = 0x00000aaaa;
+
+
+asm volatile ("stag %0, 0(%1)" ::"r"(tag_in), "r"(a));
+a[0] = 0xBBBBBBFEFEEFFFFF;
+if(a[0] != 0xBBBBBBFEFEEFFFFF)
+{
+  return -1;
+}
+if(a[1] != 0x02)
+{
+  return -1;
+}
+
+asm volatile ("ltag %0, 0(%1)":"=r"(temp):"r"(a));
+if(a[0] != 0xBBBBBBFEFEEFFFFF)
+{
+  return -1;
+}
+
+ if(temp == 0x08)
    return 0;
 else
    return -1;
