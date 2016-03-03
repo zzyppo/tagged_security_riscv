010013fbf5bea5b1e220ead2ca096ef3916f889f
Philipp Jantscher <philipp.jantscher@student.tugraz.at>
2016-03-03 08:53:29 -0800

    Tried to linux compile.. failed

--- fpga/board/kc705/examples/tag_ld_st.c
+++ fpga/board/kc705/examples/tag_ld_st.c
@@ -11,7 +11,9 @@
 int main() {
 
   long a[VECT_SIZE], i;
-  
+
+  printf("Tag test program");
+
   for(i=0; i<VECT_SIZE; i++) {
     *(a+i) = rand();
    asm volatile ("stag %0, 0(%1)" ::"r"(*(a+i)), "r"((a+i)));
@@ -21,13 +23,12 @@ int main() {
     int value = *(a+i);
     int tag;
     asm volatile ("ltag %0, 0(%1)":"=r"(tag):"r"((a+i)));
-  //  int tag = load_tag(a+i);
+   //int tag = load_tag(a+i);
     if((value % (1 << TAG_WIDTH)) != tag) {
-   // printf("Error! Wrong tag readed from a[%d], expecting %x but read %x\n",
-    //       i, value % (1 << TAG_WIDTH), tag);
+   printf("Error! Wrong tag readed from a[%d], expecting %x but read %x\n", i, value % (1 << TAG_WIDTH), tag);
     return -1;
     }
   }
-  //printf("Tag load and store tests passed.\n");
+  printf("Tag load and store tests passed.\n");
 return 0;
 }
